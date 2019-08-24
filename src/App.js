e39cdde9c25da847dfc748fb3a4324f63a9816d2
gbbenson68 <gbbenson68@gmail.com>
2019-08-23 20:20:19 -0400

    Add click handler for logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from './logo.png'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      toggleLogo: true
+    }
+  }
+
+  toggleLogo = (event) => {
+    this.setState(prevState => ({ toggleLogo: !prevState.toggleLogo }))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className={this.state.toggleLogo ? 'App-logo-static' : 'App-logo'}
+            alt="logo"
+            onClick={this.toggleLogo}
+          />
+          <h2>Welcome to React</h2>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
