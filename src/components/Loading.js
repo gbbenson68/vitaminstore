8d5212d61bb16a6d8c4b37f1f8c28fe38819d011
gbbenson68 <gbbenson68@gmail.com>
2019-08-24 23:44:15 -0400

    Minor css fixes

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,12 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => (
-  <div className='spinner'>
-    <div className='double-bounce2'></div>
-    <div className='double-bounce1'></div>
+  <div>
+    <div className='spinner'>
+      <div className='double-bounce2'></div>
+      <div className='double-bounce1'></div>
+    </div>
+    <h1>Once these circles disappear, begin scrolling!</h1>
   </div>
 )
 
