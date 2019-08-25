dbfa9853987cd32aebc159be62b5a101d14448e0
gbbenson68 <gbbenson68@gmail.com>
2019-08-24 23:14:40 -0400

    Some CSS adjustments, lazy load of card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload'
 import './Card.css'
 
 const Card = (props) => (
-  <div className={props.card.animation}>
-    <div className='front' onClick={() => props.showBack(props.card)}>
-      <img src='juice.jpg' alt='Vitamin Juice' className='card-image' />
-      <div className='container'>
+  <LazyLoad height={650} offset={-100}>
+    <div className={props.card.animation}>
+      <div className='front' onClick={() => props.showBack(props.card)}>
+        <img src='juice.jpg' alt='Vitamin Juice' className='card-image' />
+        <div className='container'>
+          <h3>Vitamin Juice <span className='price'>$24.99</span></h3>
+          <p>
+            Need a boost on your vitamins? Tired of poppoing pills?
+            Drink our vitamin-enhanced juice, available in several flavors!
+          </p>
+        </div>
+      </div>
+      <div className='container-back back' onClick={() => props.showFront(props.card)}>
         <h3>Vitamin Juice <span className='price'>$24.99</span></h3>
-        <p>
-          Need a boost on your vitamins? Tired of poppoing pills?
-          Drink our vitamin-enhanced juice, available in several flavors!
-        </p>
+        <p>{props.card.description}</p>
       </div>
     </div>
-    <div className='container-back back' onClick={() => props.showFront(props.card)}>
-      <h3>Vitamin Juice <span className='price'>$24.99</span></h3>
-      <p>{props.card.description}</p>
-    </div>
-  </div>
+  </LazyLoad>
 )
 
 export default Card
